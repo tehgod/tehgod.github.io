a03426dfcf710817dd2aea92b0fccdd6ca3df653
tehgod <44032693+tehgod@users.noreply.github.com>
2024-07-24 13:57:04 -0400

    Add files via upload
    Initial upload

---
+++ action.js
@@ -0,0 +1,8 @@
+// Author: Shelby Martin
+// File Name: action.js
+
+function action(){
+    document.getElementsByClassName("form-unsubmitted")[0].classList.add("hidden")
+    document.getElementsByClassName("form-submitted")[0].classList.remove("hidden")
+}
+
